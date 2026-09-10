5864608487110077bc384c02ce385eabf95d3408
Pollo <se.gomezl@duocuc.cl>
2026-09-10 01:08:24 -0300

    elimina codigo extra

--- JS/carrito.js
+++ JS/carrito.js
@@ -15,75 +15,6 @@ function guardarCarrito(carrito) {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-function agregarAlCarrito(nombre, precio) {
-    let carrito = obtenerCarrito();
-    let encontrado = false;
-
-    for (let i = 0; i < carrito.length; i++) {
-        if (carrito[i].nombre == nombre) {
-            carrito[i].cantidad = carrito[i].cantidad + 1;
-            encontrado = true;
-        }
-    }
-
-    if (encontrado == false) {
-        carrito.push({
-            nombre: nombre,
-            precio: precio,
-            cantidad: 1
-        });
-    }
-
-    guardarCarrito(carrito);
-    actualizarCarrito();
-    abrirCarrito();
-}
-
-function quitarProducto(posicion) {
-    let carrito = obtenerCarrito();
-    carrito.splice(posicion, 1);
-    guardarCarrito(carrito);
-    actualizarCarrito();
-}
-
-function cambiarCantidad(posicion, cambio) {
-    let carrito = obtenerCarrito();
-    carrito[posicion].cantidad = carrito[posicion].cantidad + cambio;
-
-    if (carrito[posicion].cantidad <= 0) {
-        carrito.splice(posicion, 1);
-    }
-
-    guardarCarrito(carrito);
-    actualizarCarrito();
-}
-
-function calcularTotal() {
-    let carrito = obtenerCarrito();
-    let total = 0;
-
-    for (let i = 0; i < carrito.length; i++) {
-        total = total + carrito[i].precio * carrito[i].cantidad;
-    }
-
-    return total;
-}
-
-function contarProductos() {
-    let carrito = obtenerCarrito();
-    let cantidad = 0;
-
-    for (let i = 0; i < carrito.length; i++) {
-        cantidad = cantidad + carrito[i].cantidad;
-    }
-
-    return cantidad;
-}
-
-function formatoPrecio(precio) {
-    return "$" + precio.toLocaleString("es-CL");
-}
-
 function abrirCarrito() {
     let carrito = document.getElementById("carritoLateral");
     let fondo = document.getElementById("fondoCarrito");
@@ -182,43 +113,6 @@ function mostrarCarritoPagina() {
     }
 }
 
-function actualizarTotales() {
-    let total = formatoPrecio(calcularTotal());
-    let cantidad = contarProductos();
-
-    let cantidadCarrito = document.getElementById("cantidadCarrito");
-    let cantidadPagina = document.getElementById("cantidadPagina");
-    let resumenCantidad = document.getElementById("resumenCantidad");
-
-    if (cantidadCarrito != null) {
-        cantidadCarrito.innerText = cantidad;
-    }
-
-    if (cantidadPagina != null) {
-        cantidadPagina.innerText = cantidad;
-    }
-
-    if (resumenCantidad != null) {
-        resumenCantidad.innerText = cantidad;
-    }
-
-    let ids = [
-        "totalTransferencia",
-        "totalOtros",
-        "totalCarrito",
-        "totalTransferenciaPagina",
-        "totalOtrosPagina",
-        "totalPagina"
-    ];
-
-    for (let i = 0; i < ids.length; i++) {
-        let elemento = document.getElementById(ids[i]);
-
-        if (elemento != null) {
-            elemento.innerText = total;
-        }
-    }
-}
 
 function actualizarCarrito() {
     mostrarCarritoLateral();
